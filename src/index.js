1831c3be954b85d100d9c45c27f8f4f3ee7c80c1
Yanettr <Yanettr2023@gmail.com>
2023-04-16 20:25:08 -0500

    avance card validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pantallaResultado = document.getElementById("resultado")
+    pantallaResultado.style.display = "none"
+
+    const form = document.getElementById("form");
+    form.addEventListener('submit', formularioFinalizado);
+});
+
+// creo la funcionalidad del boton de reinicio y por ultimo modifico el formulario con "".reset" para
+// limpiar los campos del formulario
+document.getElementById("botonReinicio").addEventListener("click", function(e) {
+    e.preventDefault();
+    const pantallaResultado = document.getElementById("resultado")
+    const primeraPantalla = document.getElementById("formulario")
+    primeraPantalla.style.display = "block"
+    pantallaResultado.style.display = "none"
+    document.getElementById("form").reset();
+});
+
+
+function formularioFinalizado(event) {
+    event.preventDefault();
+
+    const numeroIngresado = document.getElementById("numeroTcIngresado").value
+    const resultadoValidacion = validator.isValid(numeroIngresado)
+
+    const primeraPantalla = document.getElementById("formulario")
+    const pantallaResultado = document.getElementById("resultado")
+    const numeroEnmascarado = validator.maskify(numeroIngresado)
+
+    //con inner sobre escribo mi etiqueta <p> y traigo mi variable enmascarado
+    const numeroTarjeta = document.getElementById("numeroDeTarjeta")
+    numeroTarjeta.innerHTML = numeroEnmascarado
+
+    //se trae los datos de mes y anio y se usa innerhtml para sobreescribir en <p>
+    const vencimientoDeTarjeta = document.getElementById("fechaDeVencimiento")
+    vencimientoDeTarjeta.innerHTML = document.getElementById("mes").value + "/" + document.getElementById("anio").value
+
+    const selectTipoDeTarjeta = document.getElementById("tipodeTarjeta")
+    const parrafoTipoDeTarjeta = document.getElementById("resTipoDeTarjeta")
+    parrafoTipoDeTarjeta.innerHTML = selectTipoDeTarjeta.value;
+
+    const tipoDeBanco = document.getElementById("bancos")
+    const parrafoBanco = document.getElementById("TipoBanco")
+    parrafoBanco.innerHTML = tipoDeBanco.value
+
+
+    //usamos style.display para oculpar y que aparesca las secciones
+
+    primeraPantalla.style.display = "none"
+    pantallaResultado.style.display = "block"
+
+    const pResultadoValidacion = document.getElementById("respuesta")
+    if (resultadoValidacion) {
+        pResultadoValidacion.innerHTML = "Tarjeta validada correctamente"
+    } else {
+
+        pResultadoValidacion.innerHTML = "Tarjeta invalida, verfique informacion ingresada"
+    }
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,96 @@
+/*cree una constante validator, dentro del objeto se define la funcion isValid, para validar un
+número de tarjeta con el algoritmo luhn y la funcion maskify con la cual enmascaro los
+número de la tarjeta y dejo sin enmascarar los 4 úlitmos digitos*/
 const validator = {
-  // ...
+    /*is creditCardNumber valid?
+    const cardNumber = document.getElementById('cardNumber').value; llamo el valor de cardNumber
+    console.log(card);*/
+    isValid: (creditCardNumber) => {
+        let respuesta = false;
+        if (creditCardNumber.length >= 11) {
+            let arrayNumerico = invertirTarjeta(creditCardNumber);
+            arrayNumerico = multiplicarPares(arrayNumerico);
+            const total = sumarElementosArray(arrayNumerico);
+            if (total % 10 === 0) {
+                respuesta = true;
+            }
+        }
+        return respuesta;
+    },
+
+
+
+    maskify: (numeroTarjeta) => {
+        if (numeroTarjeta.length >= 5) {
+            const cuatroDigitos = numeroTarjeta.slice(-4);
+            const arrayNumeroTarjeta = Array.from(numeroTarjeta);
+            const numeroParaEnmascarado = arrayNumeroTarjeta.length - 4;
+            let acumuladorNumeroEnmascarado = "";
+
+            for (let i = 0; i < numeroParaEnmascarado; i++) {
+                acumuladorNumeroEnmascarado = acumuladorNumeroEnmascarado + "#";
+            }
+            return acumuladorNumeroEnmascarado + cuatroDigitos;
+        } else {
+            return numeroTarjeta;
+        }
+    },
 };
 
+// la funcion convierte el dato string a un arreglo de datos string("Array.from")
+// luego con ".reverse" invierto los datos de mi arreglo , con ".map"
+// (transforma cada item de mi array a un nuevo array item por item para darle el que deseo)
+// con "Numero()"
+function invertirTarjeta(numeroTarjeta) {
+    const arrayTarjeta = Array.from(numeroTarjeta).reverse();
+    const arrayNumerico = arrayTarjeta.map((numeroString) => //xxxxx
+        Number(numeroString)
+    );
+    return arrayNumerico;
+}
+
+// ubica los pares
+function esPar(numero) {
+    return numero % 2 === 0;
+}
+
+// la funcion multipica las ubicaciones pares del array
+function multiplicarPares(arrayNumerico) {
+    for (let indice = 0; indice < arrayNumerico.length; indice++) {
+        const indiceReal = indice + 1;
+        if (esPar(indiceReal)) {
+            arrayNumerico[indice] = arrayNumerico[indice] * 2;
+
+            if (esNumeroDosDigitos(arrayNumerico[indice])) {
+                arrayNumerico[indice] = sumarDigitos(arrayNumerico[indice]);
+            }
+        }
+    }
+    return arrayNumerico;
+}
+
+// reconocemosn si el resultado es mayor a 2 digitos (mayor o = a 10)
+function esNumeroDosDigitos(numero) {
+    return numero >= 10;
+}
+
+// sumar resultados de los resultado de dos digitos
+function sumarDigitos(numero) {
+    const numeroString = numero.toString();
+    const arrayNumero = Array.from(numeroString);
+
+    let acumulador = 0;
+    arrayNumero.forEach((element) => {
+        acumulador += Number(element);
+    });
+    return acumulador;
+}
+// sumas los indices de mis arreglos
+function sumarElementosArray(arrayNumerico) {
+    let total = 0;
+    arrayNumerico.forEach((element) => {
+        total += element;
+    });
+    return total;
+}
 export default validator;
